1bd7237123ed0f5ff1daaea4c6e8ef3e3a348b98
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2024-02-28 16:15:40 -0500

    Add Event Listeners for Popup

---
+++ app.js
@@ -0,0 +1,180 @@
+ //ONE
+ const employeeOne = document.querySelector('.employee-one');
+ const modOne = document.querySelector('.mod-one');
+
+ employeeOne.addEventListener( 'click', (e) => {
+    if (employeeOne === e.target) {
+        modOne.style.display = 'block';
+    }
+ });
+
+
+ //TWO
+ const employeeTwo = document.querySelector('.employee-two');
+ const modTwo = document.querySelector('.mod-two');
+
+ employeeTwo.addEventListener( 'click', (e) => {
+    if (employeeTwo === e.target) {
+        modTwo.style.display = 'block';
+    }
+ });
+
+
+ //THREE
+ const employeeThree = document.querySelector('.employee-three');
+ const modThree = document.querySelector('.mod-three');
+
+ employeeThree.addEventListener( 'click', (e) => {
+    if (employeeThree === e.target) {
+        modThree.style.display = 'block';
+    }
+ });
+
+
+ //FOUR
+ const employeeFour = document.querySelector('.employee-four');
+ const modFour = document.querySelector('.mod-four');
+
+ employeeFour.addEventListener( 'click', (e) => {
+    if (employeeFour === e.target) {
+        modFour.style.display = 'block';
+    }
+ });
+
+
+ //FIVE
+ const employeeFive = document.querySelector('.employee-five');
+ const modFive = document.querySelector('.mod-five');
+
+ employeeFive.addEventListener( 'click', (e) => {
+    if (employeeFive === e.target) {
+        modFive.style.display = 'block';
+    }
+ });
+
+
+ //SIX
+ const employeeSix = document.querySelector('.employee-six');
+ const modSix = document.querySelector('.mod-six');
+
+ employeeSix.addEventListener( 'click', (e) => {
+    if (employeeSix === e.target) {
+        modSix.style.display = 'block';
+    }
+ });
+
+
+ //SEVEN
+ const employeeSeven = document.querySelector('.employee-seven');
+ const modSeven = document.querySelector('.mod-seven');
+
+ employeeSeven.addEventListener( 'click', (e) => {
+    if (employeeSeven === e.target) {
+        modSeven.style.display = 'block';
+    }
+ });
+
+
+ //EIGHT
+ const employeeEight = document.querySelector('.employee-eight');
+ const modEight = document.querySelector('.mod-eight');
+
+ employeeEight.addEventListener( 'click', (e) => {
+    if (employeeEight === e.target) {
+        modEight.style.display = 'block';
+    }
+ });
+
+
+ //NINE
+ const employeeNine = document.querySelector('.employee-nine');
+ const modNine = document.querySelector('.mod-nine');
+
+ employeeNine.addEventListener( 'click', (e) => {
+    if (employeeNine === e.target) {
+        modNine.style.display = 'block';
+    }
+ });
+
+
+ //TEN
+ const employeeTen = document.querySelector('.employee-ten');
+ const modTen = document.querySelector('.mod-ten');
+
+ employeeTen.addEventListener( 'click', (e) => {
+    if (employeeTen === e.target) {
+        modTen.style.display = 'block';
+    }
+ });
+
+
+ //ELEVEN
+ const employeeEleven = document.querySelector('.employee-eleven');
+ const modEleven = document.querySelector('.mod-eleven');
+
+ employeeEleven.addEventListener( 'click', (e) => {
+    if (employeeEleven === e.target) {
+        modEleven.style.display = 'block';
+    }
+ });
+
+
+ //TWELVE
+ const employeeTwelve = document.querySelector('.employee-twelve');
+ const modTwelve = document.querySelector('.mod-twelve');
+
+ employeeTwelve.addEventListener( 'click', (e) => {
+    if (employeeTwelve === e.target) {
+        modTwelve.style.display = 'block';
+    }
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
