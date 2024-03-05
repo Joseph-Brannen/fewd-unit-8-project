6eaa20d3a4cd2517948d313c0234c96cdcac8fd4
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2024-03-05 16:09:24 -0500

    Updates to ALL

--- app.js
+++ app.js
@@ -1,213 +1,23 @@
- //ONE
- const employeeOne = document.querySelector('.employee-one');
- const modOne = document.querySelector('.mod-one');
- const buttonOne = document.getElementById('button-one');
+/* /////////////////////////////////////////////////////// 
+                EVENT LISTENERS 
+/////////////////////////////////////////////// */
+const employee = document.querySelectorAll('.employee');
+const modal = document.querySelector('.modal');
+const button = document.querySelector('.button');
 
- employeeOne.addEventListener( 'click', (e) => {
-    if (employeeOne === e.target) {
-        modOne.style.display = 'grid';
-    } 
- });
-
-modOne.addEventListener( 'click', e => {
-    if (buttonOne === e.target) {
-        modOne.style.display = 'none';
-    }
+employee.forEach( (item) => {
+    item.addEventListener( 'click', (e) => {
+        if (item === e.target) {
+            modal.style.display = 'grid';
+        }
+    });
 });
 
- //TWO
- const employeeTwo = document.querySelector('.employee-two');
- const modTwo = document.querySelector('.mod-two');
- const buttonTwo = document.getElementById('button-two');
-
- employeeTwo.addEventListener( 'click', (e) => {
-    if (employeeTwo === e.target) {
-        modTwo.style.display = 'grid';
-    }
- });
-
- modTwo.addEventListener( 'click', e => {
-    if (buttonTwo === e.target) {
-        modTwo.style.display = 'none';
+modal.addEventListener( 'click', (e) => {
+    if (button === e.target) {
+        modal.style.display = 'none';
     }
 });
-
- //THREE
- const employeeThree = document.querySelector('.employee-three');
- const modThree = document.querySelector('.mod-three');
- const buttonThree = document.getElementById('button-three');
-
- employeeThree.addEventListener( 'click', (e) => {
-    if (employeeThree === e.target) {
-        modThree.style.display = 'grid';
-    }
- });
-
- modThree.addEventListener( 'click', e => {
-    if (buttonThree === e.target) {
-        modThree.style.display = 'none';
-    }
-});
-
- //FOUR
- const employeeFour = document.querySelector('.employee-four');
- const modFour = document.querySelector('.mod-four');
- const buttonFour = document.getElementById('button-four');
-
- employeeFour.addEventListener( 'click', (e) => {
-    if (employeeFour === e.target) {
-        modFour.style.display = 'grid';
-    }
- });
-
- modFour.addEventListener( 'click', e => {
-    if (buttonFour === e.target) {
-        modFour.style.display = 'none';
-    }
-});
-
- //FIVE
- const employeeFive = document.querySelector('.employee-five');
- const modFive = document.querySelector('.mod-five');
- const buttonFive = document.getElementById('button-five');
-
- employeeFive.addEventListener( 'click', (e) => {
-    if (employeeFive === e.target) {
-        modFive.style.display = 'grid';
-    }
- });
-
- modFive.addEventListener( 'click', e => {
-    if (buttonFive === e.target) {
-        modFive.style.display = 'none';
-    }
-});
-
-
- //SIX
- const employeeSix = document.querySelector('.employee-six');
- const modSix = document.querySelector('.mod-six');
- const buttonSix = document.getElementById('button-six');
-
- employeeSix.addEventListener( 'click', (e) => {
-    if (employeeSix === e.target) {
-        modSix.style.display = 'grid';
-    }
- });
-
- modSix.addEventListener( 'click', e => {
-    if (buttonSix === e.target) {
-        modSix.style.display = 'none';
-    }
-});
-
-
- //SEVEN
- const employeeSeven = document.querySelector('.employee-seven');
- const modSeven = document.querySelector('.mod-seven');
- const buttonSeven = document.getElementById('button-seven');
-
- employeeSeven.addEventListener( 'click', (e) => {
-    if (employeeSeven === e.target) {
-        modSeven.style.display = 'grid';
-    }
- });
-
- modSeven.addEventListener( 'click', e => {
-    if (buttonSeven === e.target) {
-        modSeven.style.display = 'none';
-    }
-});
-
- //EIGHT
- const employeeEight = document.querySelector('.employee-eight');
- const modEight = document.querySelector('.mod-eight');
- const buttonEight = document.getElementById('button-eight');
-
- employeeEight.addEventListener( 'click', (e) => {
-    if (employeeEight === e.target) {
-        modEight.style.display = 'grid';
-    }
- });
-
- modEight.addEventListener( 'click', e => {
-    if (buttonEight === e.target) {
-        modEight.style.display = 'none';
-    }
-});
-
-
- //NINE
- const employeeNine = document.querySelector('.employee-nine');
- const modNine = document.querySelector('.mod-nine');
- const buttonNine = document.getElementById('button-nine');
-
- employeeNine.addEventListener( 'click', (e) => {
-    if (employeeNine === e.target) {
-        modNine.style.display = 'grid';
-    }
- });
-
- modNine.addEventListener( 'click', e => {
-    if (buttonNine === e.target) {
-        modNine.style.display = 'none';
-    }
-});
-
-
- //TEN
- const employeeTen = document.querySelector('.employee-ten');
- const modTen = document.querySelector('.mod-ten');
- const buttonTen = document.getElementById('button-ten');
-
- employeeTen.addEventListener( 'click', (e) => {
-    if (employeeTen === e.target) {
-        modTen.style.display = 'grid';
-    }
- });
-
- modTen.addEventListener( 'click', e => {
-    if (buttonTen === e.target) {
-        modTen.style.display = 'none';
-    }
-});
-
-
- //ELEVEN
- const employeeEleven = document.querySelector('.employee-eleven');
- const modEleven = document.querySelector('.mod-eleven');
- const buttonEleven = document.getElementById('button-eleven');
-
- employeeEleven.addEventListener( 'click', (e) => {
-    if (employeeEleven === e.target) {
-        modEleven.style.display = 'grid';
-    }
- });
-
- modEleven.addEventListener( 'click', e => {
-    if (buttonEleven === e.target) {
-        modEleven.style.display = 'none';
-    }
-});
-
-
- //TWELVE
- const employeeTwelve = document.querySelector('.employee-twelve');
- const modTwelve = document.querySelector('.mod-twelve');
- const buttonTwelve = document.getElementById('button-twelve');
-
- employeeTwelve.addEventListener( 'click', (e) => {
-    if (employeeTwelve === e.target) {
-        modTwelve.style.display = 'grid';
-    }
- });
-
- modTwelve.addEventListener( 'click', e => {
-    if (buttonTwelve === e.target) {
-        modTwelve.style.display = 'none';
-    }
-});
-
 
 
 
